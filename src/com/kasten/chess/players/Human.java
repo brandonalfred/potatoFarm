1247cd0d03a15180361db5f781eea5be16c95916
AlejandroCado <35822419+AlejandroCado@users.noreply.github.com>
2018-03-02 17:01:10 -0600

    Add files via upload
    just (cheaply) placed the rest of the pieces onto the board

--- src/com/kasten/chess/players/Human.java
+++ src/com/kasten/chess/players/Human.java
@@ -1,7 +1,12 @@
-package com.kasten.chess.players;
+package src.com.kasten.chess.players;
 
-import com.kasten.chess.pieces.Pawn;
-import com.kasten.chess.pieces.Piece;
+import src.com.kasten.chess.pieces.Pawn;
+import src.com.kasten.chess.pieces.Rook;
+import src.com.kasten.chess.pieces.Knight;
+import src.com.kasten.chess.pieces.King;
+import src.com.kasten.chess.pieces.Queen;
+import src.com.kasten.chess.pieces.Bishop;
+import src.com.kasten.chess.pieces.Piece;
 
 import java.util.ArrayList;
 
@@ -47,6 +52,22 @@ public class Human implements Player {
             // this is using hardcoded test values <- don't leave these!
             pieceID++;
         }
+        newPieces.add(new Rook(playerID, pieceID, 7, 0));
+        pieceID++;
+        newPieces.add(new Knight(playerID, pieceID, 7, 1));
+        pieceID++;
+        newPieces.add(new Bishop(playerID, pieceID, 7, 2));
+        pieceID++;
+        newPieces.add(new Queen(playerID, pieceID, 7, 3));
+        pieceID++;
+        newPieces.add(new King(playerID, pieceID, 7, 4 ));
+        pieceID++;
+        newPieces.add(new Bishop(playerID, pieceID, 7, 5));
+        pieceID++;
+        newPieces.add(new Knight(playerID, pieceID, 7, 6));
+        pieceID++;
+        newPieces.add(new Rook(playerID, pieceID, 7, 7));
+        
         return newPieces;
     }
 }
