8dd4500f56187db20bcea7202b869893b6c7df1a
AlejandroCado <35822419+AlejandroCado@users.noreply.github.com>
2018-03-02 17:00:22 -0600

    Add files via upload
    Added the rest of the pieces.
    Changed/added aPiece movement functions relevant to the pieces.

---
+++ src/com/kasten/chess/pieces/Queen.java
@@ -0,0 +1,33 @@
+package src.com.kasten.chess.pieces;
+
+import java.util.ArrayList;
+
+public class Queen extends aPiece {
+    public Queen(int ownerID, int pieceID, int startRow, int startCol) {
+        super(ownerID, pieceID, startRow, startCol);
+        System.out.printf("creating queen for player %d at %d, %d\n", ownerID, row, col);
+    }
+
+    @Override
+    public String getType() {
+        return "queen";
+    }
+
+    @Override
+    public ArrayList<ArrayList<Integer>> getAvailableMoves() {
+        ArrayList<ArrayList<Integer>> availableMoves = new ArrayList<>();
+
+        for (int i = 1; i <= 7; i++) {
+            availableMoves.add(goFrontRight(i));
+            availableMoves.add(goFrontLeft(i));
+            availableMoves.add(goRightBackward(i));
+            availableMoves.add(goLeftBackward(i));
+            availableMoves.add(goForward(i));
+            availableMoves.add(goBackward(i));
+            availableMoves.add(goRight(i));
+            availableMoves.add(goLeft(i));
+        }
+
+        return availableMoves;
+    }
+}
