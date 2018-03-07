3d33555eb05b3a59c825c7173ce8729322c5065b
Brandon Alfred <bca25@txstate.edu>
2018-03-06 23:25:21 -0600

    adding logic for pictures/changing board color

--- src/com/griffin/chess/utilities/Cell.java
+++ src/com/griffin/chess/utilities/Cell.java
@@ -26,16 +26,20 @@ public class Cell extends JButton {
         row = 0;
         col = 0;
         battleColor = PINK;
-        darkColor = BLACK;
-        lightColor = RED;
+        //darkColor = BLACK;
+        //lightColor = RED;
+        darkColor = new Color(139, 69, 19);
+        lightColor = new Color(238, 232, 170);
         selectedColor = GREEN;
         destColor = YELLOW;
         targetColor = BLUE;
     }
 
     private void setCellColor(String theme, String cellState) {
-        if (theme.equals("night"))
+        if (theme.equals("night")) {
             lightColor = GRAY;
+            darkColor = BLACK;
+        }
 
         String cellColor = cellState.substring(cellState.length()-1);
         if (cellColor.equals("~")) setBackground(selectedColor);
@@ -49,12 +53,99 @@ public class Cell extends JButton {
     public void setCellGraphics(String theme, String cellState) {
         setCellColor(theme, cellState);
 
-        if (cellState.substring(0,1).equals("-")) {
+        if(cellState.substring(0,1).equals("-")) {
             // eventually text here will get replaced with an image
-            setText("");
+            //setText("");
+            setIcon(null);
         } else {
-            setText(cellState.substring(1,2));
-            setForeground(WHITE);
+            //setText(cellState.substring(1,2));
+            //setForeground(WHITE);
+            if(theme.equals("night")) {
+                if(cellState != "-") {
+                    // print pictures
+                    switch ((cellState.substring(0,2))) {
+                        case "1♙":
+                            setIcon(new ImageIcon("img/chess-pieces/white-pawn.png"));
+                            break;
+                        case "1♗":
+                            setIcon(new ImageIcon("img/chess-pieces/white-bishop.png"));
+                            break;
+                        case "1♔":
+                            setIcon(new ImageIcon("img/chess-pieces/white-king.png"));
+                            break;
+                        case "1♘":
+                            setIcon(new ImageIcon("img/chess-pieces/white-knight.png"));
+                            break;
+                        case "1♕":
+                            setIcon(new ImageIcon("img/chess-pieces/white-queen.png"));
+                            break;
+                        case "1♖":
+                            setIcon(new ImageIcon("img/chess-pieces/white-rook.png"));
+                            break;
+                        case "0♙":
+                            //setIcon(new ImageIcon("img/chess-pieces/red-pawn.png"));
+                            break;
+                        case "0♗":
+                            //setIcon(new ImageIcon("img/chess-pieces/red-bishop.png"));
+                            break;
+                        case "0♔":
+                            //setIcon(new ImageIcon("img/chess-pieces/red-king.png"));
+                            break;
+                        case "0♘":
+                            //setIcon(new ImageIcon("img/chess-pieces/red-knight.png"));
+                            break;
+                        case "0♕":
+                            //setIcon(new ImageIcon("img/chess-pieces/red-queen.png"));
+                            break;
+                        case "0♖":
+                            //setIcon(new ImageIcon("img/chess-pieces/red-rook.png"));
+                            break;
+                    }
+                }
+            }
+            else {
+                if(cellState != "-") {
+                    // print pictures
+                    switch ((cellState.substring(0,2))) {
+                        case "1♙":
+                            setIcon(new ImageIcon("img/chess-pieces/white-pawn.png"));
+                            break;
+                        case "1♗":
+                            setIcon(new ImageIcon("img/chess-pieces/white-bishop.png"));
+                            break;
+                        case "1♔":
+                            setIcon(new ImageIcon("img/chess-pieces/white-king.png"));
+                            break;
+                        case "1♘":
+                            setIcon(new ImageIcon("img/chess-pieces/white-knight.png"));
+                            break;
+                        case "1♕":
+                            setIcon(new ImageIcon("img/chess-pieces/white-queen.png"));
+                            break;
+                        case "1♖":
+                            setIcon(new ImageIcon("img/chess-pieces/white-rook.png"));
+                            break;
+                        case "0♙":
+                            setIcon(new ImageIcon("img/chess-pieces/black-pawn.png"));
+                            break;
+                        case "0♗":
+                            setIcon(new ImageIcon("img/chess-pieces/black-bishop.png"));
+                            break;
+                        case "0♔":
+                            setIcon(new ImageIcon("img/chess-pieces/black-king.png"));
+                            break;
+                        case "0♘":
+                            setIcon(new ImageIcon("img/chess-pieces/black-knight.png"));
+                            break;
+                        case "0♕":
+                            setIcon(new ImageIcon("img/chess-pieces/black-queen.png"));
+                            break;
+                        case "0♖":
+                            setIcon(new ImageIcon("img/chess-pieces/black-rook.png"));
+                            break;
+                    }
+                }
+            }
         }
     }
 
@@ -64,3 +155,4 @@ public class Cell extends JButton {
         setActionCommand(Integer.toString(row) + Integer.toString(col));
     }
 }
+
